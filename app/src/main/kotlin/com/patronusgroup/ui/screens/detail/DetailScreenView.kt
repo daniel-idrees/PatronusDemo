d7934a258eebfccd5845160605be940b0a0b15d1
Daniyal Idrees <>
2023-09-12 00:59:56 +0200

    initial implementations of the screen compose views

--- app/src/main/kotlin/com/patronusgroup/ui/screens/detail/DetailScreenView.kt
+++ app/src/main/kotlin/com/patronusgroup/ui/screens/detail/DetailScreenView.kt
@@ -1,7 +1,23 @@
 package com.patronusgroup.ui.screens.detail
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.patronusgroup.ui.screens.detail.state.DetailUiState
+import com.patronusgroup.ui.screens.detail.viewmodel.DetailViewModel
+import com.patronusgroup.ui.screens.views.ErrorView
+import com.patronusgroup.ui.screens.views.LoadingView
 
 @Composable
-fun DetailScreenView() {
+fun DetailScreenView(viewModel: DetailViewModel) {
+    val viewState by viewModel.detailUiState.collectAsStateWithLifecycle()
+    when (viewState) {
+        is DetailUiState.Loading -> LoadingView()
+        is DetailUiState.Success -> DetailScreen()
+        else -> ErrorView() {}
+    }
+}
+
+@Composable
+private fun DetailScreen() {
 }
--- app/src/main/kotlin/com/patronusgroup/ui/screens/list/ListScreenView.kt
+++ app/src/main/kotlin/com/patronusgroup/ui/screens/list/ListScreenView.kt
@@ -1,7 +1,23 @@
 package com.patronusgroup.ui.screens.list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.patronusgroup.ui.screens.list.state.ListUiState
+import com.patronusgroup.ui.screens.list.viewmodel.ListViewModel
+import com.patronusgroup.ui.screens.views.ErrorView
+import com.patronusgroup.ui.screens.views.LoadingView
 
 @Composable
-fun ListScreenView() {
+fun ListScreenView(viewModel: ListViewModel, navigateToDetail: () -> Unit) {
+    val viewState by viewModel.listUiState.collectAsStateWithLifecycle()
+    when (viewState) {
+        is ListUiState.Loading -> LoadingView()
+        is ListUiState.Success -> ListScreen(navigateToDetail)
+        else -> ErrorView(viewModel::getList)
+    }
+}
+
+@Composable
+private fun ListScreen(navigateToDetail: () -> Unit) {
 }
