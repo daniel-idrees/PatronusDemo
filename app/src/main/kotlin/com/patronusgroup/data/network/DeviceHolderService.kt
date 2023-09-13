71001ca4906f82474b573ec2cb58551d23063559
Daniyal Idrees <>
2023-09-13 20:08:00 +0200

    service parameter correction

--- app/src/main/kotlin/com/patronusgroup/data/network/DeviceHolderService.kt
+++ app/src/main/kotlin/com/patronusgroup/data/network/DeviceHolderService.kt
@@ -3,12 +3,13 @@ package com.patronusgroup.data.network
 import com.patronusgroup.data.dto.DeviceHolderDetailResponse
 import com.patronusgroup.data.dto.DeviceHolderListResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface DeviceHolderService {
     @GET("users")
     suspend fun getDeviceHolderList(): DeviceHolderListResponse
 
-    @GET("users")
-    suspend fun getDeviceHolderDetail(@Query("id") id: String): DeviceHolderDetailResponse
+    @GET("users/{id}")
+    suspend fun getDeviceHolderDetail(@Path("id") id: String): DeviceHolderDetailResponse
 }
