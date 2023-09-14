1065c9aeb213f276632388bb9186b8eafb14f667
Daniyal Idrees <>
2023-09-14 03:44:47 +0200

    text styles added

--- app/src/main/kotlin/com/patronusgroup/ui/theme/Type.kt
+++ app/src/main/kotlin/com/patronusgroup/ui/theme/Type.kt
@@ -13,22 +13,68 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val HeaderTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = BlackText,
+    fontSize = 27.sp,
+    lineHeight = 32.4.sp,
+    fontWeight = FontWeight.Bold,
+)
+
+val FullNameTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = BlackText,
+    fontWeight = FontWeight.Bold,
+    fontSize = 17.sp,
+    lineHeight = 20.4.sp,
+)
+
+val GenderTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = GreyText,
+    fontSize = 17.sp,
+    lineHeight = 20.4.sp,
+)
+
+val BodyTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = BlackBodyText,
+    fontSize = 17.sp,
+    lineHeight = 20.4.sp,
+)
+
+val ImagePlaceHolderTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = GreyText,
+    fontWeight = FontWeight.Bold,
+    fontSize = 22.sp,
+    lineHeight = 26.4.sp,
+)
+
+val RedStickerTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = RedStickerText,
+    fontWeight = FontWeight.Normal,
+    fontSize = 13.sp,
+    lineHeight = 15.6.sp,
+)
+
+val GreyStickerTextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = GreyText,
+    fontWeight = FontWeight.Normal,
+    fontSize = 13.sp,
+    lineHeight = 15.6.sp,
+)
+
+val Header2TextStyle = TextStyle(
+    fontFamily = rubikFamily,
+    color = BlackBodyText,
+    fontWeight = FontWeight.Bold,
+    fontSize = 15.sp,
+    lineHeight = 18.sp,
 )
