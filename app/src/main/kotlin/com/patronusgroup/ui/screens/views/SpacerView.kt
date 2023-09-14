f6967fc8886b6ef9a5bb80a42d77877ed9598cae
Daniyal Idrees <>
2023-09-14 03:49:59 +0200

    common view for spacers

---
+++ app/src/main/kotlin/com/patronusgroup/ui/screens/views/SpacerView.kt
@@ -0,0 +1,22 @@
+package com.patronusgroup.ui.screens.views
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerM() {
+    Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Composable
+fun SpacerS() {
+    Spacer(modifier = Modifier.height(8.dp))
+}
+
+@Composable
+fun SpacerXS() {
+    Spacer(modifier = Modifier.height(5.dp))
+}
