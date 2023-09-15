9d6bdf9379292e5951e6e25a15419ecaeb1bb99f
Daniyal Idrees <>
2023-09-16 00:05:36 +0200

    screen navigation implementations moved to a separate file

--- presentation/src/main/kotlin/com/patronusgroup/presentation/detail/navigation/DetailNavigation.kt
+++ presentation/src/main/kotlin/com/patronusgroup/presentation/detail/navigation/DetailNavigation.kt
@@ -1,20 +1,26 @@
 package com.patronusgroup.presentation.detail.navigation
 
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navOptions
 import com.patronusgroup.presentation.nav.DetailScreen
 import com.patronusgroup.presentation.nav.detailScreenArgumentIdKey
 
 fun NavGraphBuilder.detailScreen(
-    content: @Composable (NavBackStackEntry) -> Unit,
+    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit,
 ) = composable(DetailScreen.route, content = content)
 
-fun NavController.navigateToDetail(id: String, navOptions: NavOptions? = null) {
-    this.navigate(DetailScreen.routeTo(id), navOptions)
+fun NavController.navigateToDetail(id: String) {
+    this.navigate(
+        DetailScreen.routeTo(id),
+        navOptions {
+            this.launchSingleTop = true
+        },
+    )
 }
 
 private fun DetailScreen.routeTo(id: String) =
--- presentation/src/main/kotlin/com/patronusgroup/presentation/list/navigation/ListNavigation.kt
+++ presentation/src/main/kotlin/com/patronusgroup/presentation/list/navigation/ListNavigation.kt
@@ -1,17 +1,23 @@
 package com.patronusgroup.presentation.list.navigation
 
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navOptions
 import com.patronusgroup.presentation.nav.ListScreen
 
 fun NavGraphBuilder.listScreen(
-    content: @Composable (NavBackStackEntry) -> Unit,
+    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit,
 ) = composable(ListScreen.route, content = content)
 
-fun NavController.navigateToList(navOptions: NavOptions? = null) {
-    this.navigate(ListScreen.route, navOptions)
+fun NavController.navigateToList() {
+    this.navigate(
+        ListScreen.route,
+        navOptions {
+            this.launchSingleTop = true
+        },
+    )
 }
