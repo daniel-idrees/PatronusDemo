a1e7889f22d0ad19e1cd40c624eb04762295235f
Daniyal Idrees <>
2023-09-16 00:43:28 +0200

    network monitor injected and main composable view added to the content

--- app/src/main/kotlin/com/patronusgroup/ui/MainActivity.kt
+++ app/src/main/kotlin/com/patronusgroup/ui/MainActivity.kt
@@ -3,15 +3,20 @@ package com.patronusgroup.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.patronusgroup.ui.nav.DeviceApp
+import com.patronusgroup.core.util.NetworkMonitor
+import dagger.Lazy
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var networkMonitor: Lazy<NetworkMonitor>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DeviceApp()
+            PatronusDemoApp(networkMonitor.get())
         }
     }
 }
